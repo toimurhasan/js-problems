b57d954107b0ddfb8da6b17c05f82ba07bdebd5d
toimurhasan <2imur.hasan@gmail.com>
2025-08-20 17:29:23 +0600

    reverse a string

--- solutions.js
+++ solutions.js
@@ -1,11 +1,5 @@
 // Problem 1: Reverse a String
-function reverseString(str) {
-  let reversed = "";
-  for (let i = str.length - 1; i >= 0; i--) {
-    reversed += str[i];
-  }
-  return reversed;
-}
+
 
 // Problem 2: Count Vowels in a String
 function countVowels(str) {
