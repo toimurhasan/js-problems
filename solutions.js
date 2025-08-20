b597161e27eb97b94f462908bd33a8d95ae1f4ef
toimurhasan <2imur.hasan@gmail.com>
2025-08-20 17:50:38 +0600

    find the factorial of a number

--- solutions.js
+++ solutions.js
@@ -23,13 +23,7 @@
 
 
 // Problem 9: Find the Factorial of a Number
-function factorial(n) {
-  let result = 1;
-  for (let i = 1; i <= n; i++) {
-    result *= i;
-  }
-  return result;
-}
+
 
 // Problem 10: PingPong Challenge
 function pingPong() {
