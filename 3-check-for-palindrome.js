5160431442b5b76f4a1dc618acf0900ccb0f58d7
toimurhasan <2imur.hasan@gmail.com>
2025-08-20 17:34:38 +0600

    check for palindrome

---
+++ 3-check-for-palindrome.js
@@ -0,0 +1,7 @@
+function isPalindrome(str) {
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return str === reversed;
+}
--- solutions.js
+++ solutions.js
@@ -5,13 +5,7 @@
 
 
 // Problem 3: Check for Palindrome
-function isPalindrome(str) {
-  let reversed = "";
-  for (let i = str.length - 1; i >= 0; i--) {
-    reversed += str[i];
-  }
-  return str === reversed;
-}
+
 
 // Problem 4: Find the Maximum Number
 function findMax(arr) {
